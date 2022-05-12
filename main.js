e9920a6be4fa627e07c4d8a1bd1f31a5147aa8b9
arif <arif-celik56@hotmail.com>
2022-05-12 19:48:59 +0300

    ejs eklendi ve yönlendirmeler yapıldı

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const ejs = require('ejs');
 const app = express();
 const port = 5000;
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send({ id: 1, title: "Blog title", description: "Blog description" })
+    res.render('index');
+})
+
+app.get('/:url', (req, res) => {
+    res.render(req.params.url);
+    console.log(req.params);
 })
 
 app.listen(port, () => {

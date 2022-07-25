21ba513de978247347a1dac0f700f96d1cb1c3f7
arif <arif-celik56@hotmail.com>
2022-07-25 21:14:53 +0300

    3. bölüm

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
 const express = require('express')
-const ejs = require('ejs');
+const mongoose = require('mongoose')
 const app = express();
-const port = 5000;
+
+const Post = require('./models/Post');
+
+const port = process.argv[2] || 5000;
+
+mongoose.connect('mongodb://127.0.0.1:27017/cleanblog-test-db', { useNewUrlParser: true, useUnifiedTopology: true }, err => {
+    if (err) throw err
+    console.log('veri tabanına bağlantı başarılı');
+})
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const data = await Post.find({})
+    res.render('index', { data });
 })
 
 app.get('/:url', (req, res) => {
     res.render(req.params.url);
     console.log(req.params);
+})
+
+app.post('/add', async (req, res) => {
+    console.log(req.body);
+    await Post.create(req.body)
+    res.redirect('/');
 })
 
 app.listen(port, () => {
---
+++ models/Post.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const  PostSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        default: 'dr. watson'
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('content', PostSchema)

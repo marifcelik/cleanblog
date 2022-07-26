e31a23fc2eed9747dd12cd03b2f296965699fc09
arif <arif-celik56@hotmail.com>
2022-07-26 14:01:25 +0300

    post sayfaları eklendi

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const app = express();
 
 const Post = require('./models/Post');
 
+const host = 'localhost';
 const port = process.argv[2] || 5000;
 
 mongoose.connect('mongodb://127.0.0.1:27017/cleanblog-test-db', { useNewUrlParser: true, useUnifiedTopology: true }, err => {
@@ -22,16 +23,20 @@ app.get('/', async (req, res) => {
 })
 
 app.get('/:url', (req, res) => {
+    if (req.params.url == 'index' || req.params.url == 'post')
+        res.redirect('/');
     res.render(req.params.url);
     console.log(req.params);
 })
 
 app.post('/add', async (req, res) => {
-    console.log(req.body);
     await Post.create(req.body)
     res.redirect('/');
 })
 
-app.listen(port, () => {
-    console.log(`localhost ${port} portundan dinleniyor.`);
+app.get('/posts/:id', async (req, res)=>{
+    const post = await Post.findById(req.params.id)
+    res.render('post.ejs', { post })
 })
+
+app.listen(port, host, () => console.log(`${host}:${port} dinleniyor`));
